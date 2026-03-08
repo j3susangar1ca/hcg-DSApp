2e0f542c265d6ea086a3451d9623819080e1bb68
j3susangar1ca <j3susangar1ca@gmail.com>
2026-03-08 04:47:15 -0600

    3

--- src/Domain/obj/Debug/net10.0/GestionDocumental.Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net10.0/GestionDocumental.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionDocumental.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e11429ca48b48f05cabfc5d212ff494154d409a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b06aafd5776765e52657889c949cf25301e67c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionDocumental.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionDocumental.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
